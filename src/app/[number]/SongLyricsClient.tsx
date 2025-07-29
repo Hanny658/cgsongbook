58ec1913f7e10753f582a98077751d9d39ce8ec9
Hanny658 <hannyanhai@gmail.com>
2025-07-29 11:36:22 +1000

    Fixing Invalid Page output

--- src/app/[number]/SongLyricsClient.tsx
+++ src/app/[number]/SongLyricsClient.tsx
@@ -12,7 +12,10 @@ import SettingsButton from '../configs/settings-button'
 import { useConfig } from '../configs/settings'
 import { transposeChordString } from '../configs/chord-transpose'
 
-import { bgImages } from '../page'
+const bgImages = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', 
+  '7.jpg', '8.jpg', '9.webp', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.webp', 
+  '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg', '22.jpg', 
+  '23.jpg', '25.webp', '24.jpg', '26.jpg']
 
 export default function SongLyricsPage({ number }: { number: string | number }) {
   const [song, setSong] = useState<SongData | null>(null)
